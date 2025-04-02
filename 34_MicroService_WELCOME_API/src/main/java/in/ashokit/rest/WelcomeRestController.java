8b8fb47a269284b3141934c5794d831dd691ceea
KANHU CHARAN BEHERA <beherakanha029@gmail.com>
2025-04-02 16:03:41 +0530

    Add files via upload

---
+++ 34_MicroService_WELCOME_API/src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -0,0 +1,13 @@
+package in.ashokit.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	
+	@GetMapping("/welcome")
+	public String getWelcomeMsg() {
+		return "Wellcome to Ashok IT";
+	}
+}
